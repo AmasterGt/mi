9d968dc4d2888b2c788ddb6ebbc2a8e4be59b252
刘栋 <github88888@163.com>
2017-07-21 08:50:40 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function(){
+﻿$(function(){
 	var swiper=new Swiper(".swiper-container",{
 		 loop:true,
             autoplay:3000,
@@ -10,11 +10,34 @@ $(function(){
 			nextButton:'.swiper-button-next'
 	})
 	$(".nav_div").width($(document).width())
-//	$(".daohang ul li").hover(function(){
-//		$(".nav_div").height("230px");
-//		$(".nav_div").eq($(this).index()).css({"display":"block"})
-//	},function(){
-//		$(".nav_div").height("0");
-//		$(".nav_div").eq($(this).index()).css({"display":"none"})
-//	})
+	var lis=$(".section2 li:last")
+	console.log(lis)
+	$(".section2 ul li").not($(".section2 ul li:last-of-type")).hover(function(){
+		$(this).css({"transform":"translateY(-3px)","box-shadow":"0 5px 25px 0px #CCCCCC"})
+		$(this).find(".pinglun").css("height","25%")
+	},function(){
+		$(this).css({"transform":"translateY(0)","box-shadow":"none"})
+		$(this).find(".pinglun").css("height","0")
+	})
+		
+			$(".section1 ul li").not($(".section1 ul li:last-of-type")).hover(function(){
+		$(this).css({"transform":"translateY(-3px)","box-shadow":"0 5px 25px 0px #CCCCCC"})
+		$(this).find(".pinglun").css("height","25%")
+	},function(){
+		$(this).css({"transform":"translateY(0)","box-shadow":"none"})
+		$(this).find(".pinglun").css("height","0")
+	})
+	$(".section2 ul li:last-of-type>div").hover(function(){
+		$(this).css({"transform":"translateY(-3px)","box-shadow":"0 5px 25px 0px #CCCCCC"})
+		
+	},function(){
+		$(this).css({"transform":"translateY(0)","box-shadow":"none"})
+	})
+	$(".section1 ul li:last-of-type>div").hover(function(){
+		$(this).css({"transform":"translateY(-3px)","box-shadow":"0 5px 25px 0px #CCCCCC"})
+	},function(){
+		$(this).css({"transform":"translateY(0)","box-shadow":"none"})
+	})
+	$(".section2 ul li").not($(".section2 ul li:last-of-type")).append($("<p class='pinglun'>asdasdqweasdqweqwe<span>小小果评论</span></p>"))
+	$(".section1 ul li").not($(".section2 ul li:last-of-type")).append($("<p class='pinglun'>asdasdqweasdqweqwe<span>小小果评论</span></p>"))
 })
